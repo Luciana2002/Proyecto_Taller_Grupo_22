f5206510e7f9e38ad13abdad94de47e5b48f2a9c
Luciana Itati Fernandez Lezcano <118312382+Luciana2002@users.noreply.github.com>
2024-10-30 10:57:23 -0300

    Update SQLQuery_Taller.sql

--- SQLQuery_Taller.sql
+++ SQLQuery_Taller.sql
@@ -3,12 +3,42 @@
 -------------------------------------------
 
 CREATE DATABASE taller_db_1;
+go
 
 USE taller_db_1;
+go
 
 ----------------------------------------
 ---------- CREACIÓN DE TABLAS ----------
 ----------------------------------------
+
+if object_id('Detalle_Venta') is not null DROP TABLE Detalle_Venta;
+go
+---------------------
+if object_id('Venta') is not null DROP TABLE Venta;
+go
+---------------------
+if object_id('Cliente') is not null DROP TABLE Cliente;
+go
+---------------------
+if object_id('Producto') is not null DROP TABLE Producto;
+go
+---------------------
+if object_id('Tipo_Venta') is not null DROP TABLE Tipo_Venta;
+go
+---------------------
+if object_id('Usuario') is not null DROP TABLE Usuario;
+go
+---------------------
+if object_id('Perfil') is not null DROP TABLE Perfil;
+go
+---------------------
+if object_id('Categoria') is not null DROP TABLE Categoria;
+go
+---------------------
+if object_id('Persona') is not null DROP TABLE Persona;
+go
+---------------------
 
 CREATE TABLE Persona
 (
@@ -24,22 +54,25 @@ CREATE TABLE Persona
   CONSTRAINT PK_Persona_id PRIMARY KEY (id_persona),
   CONSTRAINT UQ_Persona_email UNIQUE (email),
   CONSTRAINT UQ_Persona_dni UNIQUE (dni),
-  CONSTRAINT UQ_Persona_telefono UNIQUE (telefono),
-);
+  CONSTRAINT UQ_Persona_telefono UNIQUE (telefono)
+)
+go
 
 CREATE TABLE Categoria
 (
   id_categoria INT NOT NULL,
   descripcion VARCHAR(50) NOT NULL,
   CONSTRAINT PK_Categoria_id PRIMARY KEY (id_categoria)
-);
+)
+go
 
 CREATE TABLE Perfil
 (
   id_perfil INT NOT NULL,
   descripcion VARCHAR(20) NOT NULL,
   CONSTRAINT PK_Perfil_id PRIMARY KEY (id_perfil)
-);
+)
+go
 
 CREATE TABLE Usuario
 (
@@ -50,14 +83,16 @@ CREATE TABLE Usuario
   CONSTRAINT PK_Usuario_id PRIMARY KEY (id_usuario),
   CONSTRAINT FK_Usuario_id_persona FOREIGN KEY (id_usuario) REFERENCES Persona(id_persona),
   CONSTRAINT FK_Usuario_id_perfil FOREIGN KEY (id_perfil) REFERENCES Perfil(id_perfil)
-);
+)
+go
 
 CREATE TABLE Tipo_Venta
 (
   id_tipo INT NOT NULL,
   descripcion VARCHAR(50) NOT NULL,
   CONSTRAINT PK_Tipo_Venta_id PRIMARY KEY (id_tipo)
-);
+)
+go
 
 CREATE TABLE Producto
 (
@@ -71,14 +106,16 @@ CREATE TABLE Producto
   CONSTRAINT PK_Producto_id PRIMARY KEY (id_producto),
   CONSTRAINT FK_Producto_id_categoria FOREIGN KEY (id_categoria) REFERENCES Categoria(id_categoria),
   CONSTRAINT UQ_Producto_nombre UNIQUE (nombre_producto)
-);
+)
+go
 
 CREATE TABLE Cliente
 (
   id_cliente INT NOT NULL,
   CONSTRAINT PK_Cliente_id PRIMARY KEY (id_cliente),
   CONSTRAINT FK_Cliente_id_persona FOREIGN KEY (id_cliente) REFERENCES Persona(id_persona)
-);
+)
+go
 
 CREATE TABLE Venta
 (
@@ -92,7 +129,8 @@ CREATE TABLE Venta
   CONSTRAINT FK_Venta_id_usuario FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
   CONSTRAINT FK_Venta_id_tipo FOREIGN KEY (id_tipo) REFERENCES Tipo_Venta(id_tipo),
   CONSTRAINT FK_Venta_id_cliente FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
-);
+)
+go
 
 CREATE TABLE Detalle_Venta
 (
@@ -104,7 +142,8 @@ CREATE TABLE Detalle_Venta
   CONSTRAINT PK_Detalle_Venta_id PRIMARY KEY (id_detalle),
   CONSTRAINT FK_Detalle_Venta_id_producto FOREIGN KEY (id_producto) REFERENCES Producto(id_producto),
   CONSTRAINT FK_Detalle_Venta_id_venta FOREIGN KEY (id_venta) REFERENCES Venta(id_venta)
-);
+)
+go
 
 -----------------------------------
 ---------- RESTRICCIONES ----------
