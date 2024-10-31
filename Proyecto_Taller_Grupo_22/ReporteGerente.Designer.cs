fb38b94ebb975ac5c14a3c403c9ef3a416117633
Luci <luciana_itati_fl@hotmail.com>
2024-10-31 16:55:52 -0300

    Resuelto conflicto en ReporteG.Designer.cs, añadido ReporteGerente y eliminado archivos obsoletos

---
+++ Proyecto_Taller_Grupo_22/ReporteGerente.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Proyecto_Taller_Grupo_22
+{
+    partial class ReporteGerente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvproductos = new DataGridView();
+            button5 = new Button();
+            panel1 = new Panel();
+            label5 = new Label();
+            label4 = new Label();
+            dtpFechaFin = new DateTimePicker();
+            dtpFechaInicio = new DateTimePicker();
+            label2 = new Label();
+            label1 = new Label();
+            label3 = new Label();
+            button4 = new Button();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label6 = new Label();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvproductos).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dgvproductos
+            // 
+            dgvproductos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvproductos.Location = new Point(29, 460);
+            dgvproductos.Name = "dgvproductos";
+            dgvproductos.RowHeadersWidth = 51;
+            dgvproductos.Size = new Size(958, 458);
+            dgvproductos.TabIndex = 116;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Microsoft Sans Serif", 12F);
+            button5.Location = new Point(626, 396);
+            button5.Name = "button5";
+            button5.Size = new Size(164, 34);
+            button5.TabIndex = 115;
+            button5.Text = "Reporte Ventas";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Violet;
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(dtpFechaFin);
+            panel1.Controls.Add(dtpFechaInicio);
+            panel1.Location = new Point(223, 112);
+            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(567, 133);
+            panel1.TabIndex = 114;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(403, 25);
+            label5.Margin = new Padding(7, 0, 7, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(80, 29);
+            label5.TabIndex = 4;
+            label5.Text = "Hasta:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(83, 25);
+            label4.Margin = new Padding(7, 0, 7, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(90, 29);
+            label4.TabIndex = 3;
+            label4.Text = "Desde:";
+            // 
+            // dtpFechaFin
+            // 
+            dtpFechaFin.Font = new Font("Microsoft Sans Serif", 9.75F);
+            dtpFechaFin.Format = DateTimePickerFormat.Short;
+            dtpFechaFin.Location = new Point(355, 61);
+            dtpFechaFin.Margin = new Padding(3, 4, 3, 4);
+            dtpFechaFin.Name = "dtpFechaFin";
+            dtpFechaFin.Size = new Size(164, 26);
+            dtpFechaFin.TabIndex = 1;
+            // 
+            // dtpFechaInicio
+            // 
+            dtpFechaInicio.Font = new Font("Microsoft Sans Serif", 9.75F);
+            dtpFechaInicio.Format = DateTimePickerFormat.Short;
+            dtpFechaInicio.Location = new Point(42, 61);
+            dtpFechaInicio.Margin = new Padding(3, 4, 3, 4);
+            dtpFechaInicio.Name = "dtpFechaInicio";
+            dtpFechaInicio.Size = new Size(164, 26);
+            dtpFechaInicio.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(435, 75);
+            label2.Margin = new Padding(7, 0, 7, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(153, 29);
+            label2.TabIndex = 113;
+            label2.Text = "Elegir fechas";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(445, 20);
+            label1.Margin = new Padding(7, 0, 7, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(133, 31);
+            label1.TabIndex = 112;
+            label1.Text = "Reportes";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft Sans Serif", 11.25F);
+            label3.Location = new Point(417, 258);
+            label3.Margin = new Padding(6, 0, 6, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(189, 24);
+            label3.TabIndex = 111;
+            label3.Text = "Generar Reporte por:";
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Microsoft Sans Serif", 12F);
+            button4.Location = new Point(522, 396);
+            button4.Name = "button4";
+            button4.Size = new Size(98, 34);
+            button4.TabIndex = 110;
+            button4.Text = "Buscar";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Microsoft Sans Serif", 12F);
+            textBox2.Location = new Point(296, 398);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(220, 30);
+            textBox2.TabIndex = 109;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Microsoft Sans Serif", 12F);
+            textBox3.Location = new Point(223, 398);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(67, 30);
+            textBox3.TabIndex = 108;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(223, 367);
+            label6.Name = "label6";
+            label6.Size = new Size(104, 25);
+            label6.TabIndex = 107;
+            label6.Text = "Vendedor:";
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Microsoft Sans Serif", 11.25F);
+            button3.Location = new Point(583, 297);
+            button3.Name = "button3";
+            button3.Size = new Size(137, 45);
+            button3.TabIndex = 106;
+            button3.Text = "Total ventas";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Microsoft Sans Serif", 11.25F);
+            button2.Location = new Point(440, 297);
+            button2.Name = "button2";
+            button2.Size = new Size(137, 45);
+            button2.TabIndex = 105;
+            button2.Text = "Más vendidos";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Microsoft Sans Serif", 11.25F);
+            button1.Location = new Point(297, 297);
+            button1.Name = "button1";
+            button1.Size = new Size(137, 45);
+            button1.TabIndex = 104;
+            button1.Text = "Recaudación";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // ReporteGerente
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1018, 739);
+            Controls.Add(dgvproductos);
+            Controls.Add(button5);
+            Controls.Add(panel1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(label3);
+            Controls.Add(button4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox3);
+            Controls.Add(label6);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "ReporteGerente";
+            Text = "ReporteGerente";
+            ((System.ComponentModel.ISupportInitialize)dgvproductos).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvproductos;
+        private Button button5;
+        private Panel panel1;
+        private Label label5;
+        private Label label4;
+        private DateTimePicker dtpFechaFin;
+        private DateTimePicker dtpFechaInicio;
+        private Label label2;
+        private Label label1;
+        private Label label3;
+        private Button button4;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label6;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+    }
+}
